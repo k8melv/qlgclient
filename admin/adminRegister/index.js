f3717b162fbf21eb52a1274c7869be7f0018b914
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-12-03 19:19:46 -0600

    update

--- admin/adminRegister/index.js
+++ admin/adminRegister/index.js
@@ -7,31 +7,38 @@ function registerAccount(){
     const date = document.getElementById(`date`).value;
     const position = document.getElementById(`position`).value;
 
-    if(document.getElementById("termsAndCond").checked==true){
-        fetch(customerApiUrl, {
-            method: "POST",
-            headers: {
-                "Accept": 'application/json',
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password,
-                position: position,
-                startdate: date,
-            })
-        }).then((response)=>{
-            console.log(response);
-            var user = {firstName, lastName, email, password, position, date};
-            sessionStorage.setItem('user', JSON.stringify(user));
-            window.location.replace("../adminProfile/adminProfile.html");
-        })
+    if (firstName == null || firstName == "" || lastName == null || lastName == "" || email == null || email == "" || password == null || password == "" || date == null || date == "" || position == null || position == ""){
+        var errorMessage = document.getElementById("errorMsg");
+        var html = "You must fill out all information";
+        errorMessage.innerHTML=html;
     }
     else{
-        var errorMessage = document.getElementById("errorMsg");
-        var html = "You must agree to the Terms and Conditions before registering your account.";
-        errorMessage.innerHTML=html;
+        if(document.getElementById("termsAndCond").checked==true){
+            fetch(customerApiUrl, {
+                method: "POST",
+                headers: {
+                    "Accept": 'application/json',
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                    position: position,
+                    startdate: date,
+                })
+            }).then((response)=>{
+                console.log(response);
+                var user = {firstName, lastName, email, password, position, date};
+                sessionStorage.setItem('user', JSON.stringify(user));
+                window.location.replace("../adminProfile/adminProfile.html");
+            })
+        }
+        else{
+            var errorMessage = document.getElementById("errorMsg");
+            var html = "You must agree to the Terms and Conditions before registering your account.";
+            errorMessage.innerHTML=html;
+        }
     }
 }
