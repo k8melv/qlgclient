2e0be4fa9402d9f93be36d390e8d90cc77b73a45
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-12-03 18:09:34 -0600

    update

--- admin/adminGardens/adminGarden.js
+++ admin/adminGardens/adminGarden.js
@@ -64,7 +64,7 @@ function putGarden(id){
       body: JSON.stringify({
         gardenID: id,
         gardenType: gardenType,
-        gardenInfo: gardenInfo  
+        gardenInformation: gardenInfo  
       })
   }).then((response)=>{
       if (response.status == 200){
