f2ea2e1bce4551ba3a9cf0f9809b4c52af98d60b
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-12-03 18:07:36 -0600

    update

--- admin/adminGardens/adminGarden.js
+++ admin/adminGardens/adminGarden.js
@@ -16,6 +16,7 @@ const GetGarden = async (id) => {
 }
 
 function displayModal(data){
+  console.log(data);
   var garden = document.getElementById("exampleModal");
   var html = `<div class="modal-dialog"><div class="modal-content">`;
   html += `<div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">${data[0].gardenType}</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>`;
