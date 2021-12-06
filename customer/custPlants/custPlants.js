6aa114cee20cb8a6f159a48cdf6b4c4f225e3f1b
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-12-06 17:37:42 -0600

    update

--- customer/custPlants/custPlants.js
+++ customer/custPlants/custPlants.js
@@ -103,6 +103,7 @@ function removeProduct(id){
     console.log(items);
     for (var i =0; i< items.length; i++) {
         var item = JSON.parse(items[i]);
+        console.log(item)
         if (item.plantID == id) {
             console.log(item)
             items.splice(item.plantID, 1);
