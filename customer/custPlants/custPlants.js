fea88557b9eb8e1710aa7b3dfcebd69a9df78a80
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-12-06 19:58:53 -0600

    update

--- customer/custPlants/custPlants.js
+++ customer/custPlants/custPlants.js
@@ -130,7 +130,7 @@ window.removeProduct = removeProduct;
 
 function cartModal(){
     var cart = []
-	cart.push(JSON.parse(sessionStorage.getItem("myCart")));
+	cart.push(sessionStorage.getItem("myCart"));
     console.log(cart);
     if (cart === null || cart === "null"){
         var html = `<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Cart</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>`
